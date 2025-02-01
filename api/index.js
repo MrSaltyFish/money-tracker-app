30f738968af754e0fa7e243c6847ecf186ce6406
MrSaltyFish <anveshkhode7588@gmail.com>
2025-02-01 15:49:07 +0530

    backend package.json

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import Transaction from './models/Transaction.js';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import Transaction from "./models/Transaction.js";
+import dotenv from "dotenv";
 
 dotenv.config(); // Load .env variables
 
@@ -10,11 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/test', (req, res) => {
-  res.json('test ok42');
+app.get("/api/test", (req, res) => {
+  res.json("test ok42");
 });
 
-app.post('/api/transaction', async (req, res) => {
+app.post("/api/transaction", async (req, res) => {
   // eslint-disable-next-line no-undef
   await mongoose.connect(process.env.MONGO_URL);
   const { price, name, description, datetime } = req.body;
@@ -27,16 +27,16 @@ app.post('/api/transaction', async (req, res) => {
   res.json(transac);
 });
 
-app.get('/api/transactions', async (req, res) => {
+app.get("/api/transactions", async (req, res) => {
   // eslint-disable-next-line no-undef
   await mongoose.connect(process.env.MONGO_URL);
 
-  console.log('/api/transactions accessed');
+  console.log("/api/transactions accessed");
 
   const transactions = await Transaction.find();
   res.json(transactions);
 });
 
 app.listen(4040, () => {
-  console.log('Server running on http://localhost:4040');
+  console.log("Server running on http://localhost:4040");
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 // import { response } from 'express';
 
 function App() {
-  const [name, setName] = useState('');
-  const [datetime, setDatetime] = useState('');
-  const [description, setDesc] = useState('');
+  const [name, setName] = useState("");
+  const [datetime, setDatetime] = useState("");
+  const [description, setDesc] = useState("");
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
@@ -14,20 +14,20 @@ function App() {
   }, []);
 
   async function getTransactions() {
-    const url = import.meta.env.VITE_API_URL + '/transactions';
+    const url = import.meta.env.VITE_API_URL + "/transactions";
     const response = await fetch(url);
     return await response.json();
   }
 
   function addNewTransaction(/*ev*/) {
     // ev.preventDefault();
-    const url = import.meta.env.VITE_API_URL + '/transaction';
+    const url = import.meta.env.VITE_API_URL + "/transaction";
 
-    const price = name.split(' ')[0];
+    const price = name.split(" ")[0];
     console.log(url);
     fetch(url, {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
+      method: "POST",
+      headers: { "Content-type": "application/json" },
       body: JSON.stringify({
         price,
         name: name.substring(price.length + 1),
@@ -36,10 +36,10 @@ function App() {
       }),
     }).then((response) => {
       response.json().then((json) => {
-        setName('');
-        setDatetime('');
-        setDesc('');
-        console.log('result', json);
+        setName("");
+        setDatetime("");
+        setDesc("");
+        console.log("result", json);
       });
     });
   }
@@ -51,14 +51,14 @@ function App() {
 
   balance = balance.toFixed(2);
 
-  const fraction = balance.split('.')[1];
-  balance = balance.split('.')[0];
+  const fraction = balance.split(".")[1];
+  balance = balance.split(".")[0];
 
   return (
     <>
       <main>
         <h1>
-          ${balance}
+          ₹{balance}
           <span>.{fraction}</span>
         </h1>
         <form onSubmit={addNewTransaction}>
@@ -67,7 +67,7 @@ function App() {
               type="text"
               value={name}
               onChange={(ev) => setName(ev.target.value)}
-              placeholder={'+200 new Samsung TV'}
+              placeholder={"+200 new Samsung TV"}
             />
             <input
               type="datetime-local"
@@ -80,7 +80,7 @@ function App() {
               type="text"
               value={description}
               onChange={(ev) => setDesc(ev.target.value)}
-              placeholder={'Description'}
+              placeholder={"Description"}
             />
           </div>
           <button type="submit">Add new Transaction</button>
@@ -99,9 +99,9 @@ function App() {
                 </div>
                 <div className="right">
                   <div
-                    className={`price ${transaction.price < 0 ? 'red' : 'green'}`}
+                    className={`price ${transaction.price < 0 ? "red" : "green"}`}
                   >
-                    {transaction.price}
+                    Rs. {transaction.price}
                   </div>
                   <div className="datetime">{transaction.datetime}</div>
                 </div>
